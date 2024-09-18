7b7a23fd23cf484be937fa1fcf11727873fad975
arman ismail <armanismail544@gmail.com>
2024-09-18 09:24:48 +0300

    creating database

--- __init__.py
+++ __init__.py
@@ -1,25 +1,16 @@
-import os
 
-from dotenv import load_dotenv
 from psycopg2 import pool
 
-load_dotenv()
-
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
 
 try:
     connection_pool = pool.SimpleConnectionPool(
         1,
         20,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT,
+        dbname="concerts",
+        user="abdirahman",
+        password="37571598a",
+        host="localhost",
+        port="5432",
     )
 
     if connection_pool:
